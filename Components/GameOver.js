1f5203ab6efb516449b2af31d60718ace3066942
rayqi <qi.raymond@gmail.com>
2018-06-17 19:51:01 -0400

    game over page works

--- Components/GameOver.js
+++ Components/GameOver.js
@@ -4,25 +4,54 @@ import { createStackNavigator } from 'react-navigation';
 
 const GameOver = ({ navigation }) => (
     <View style={styles.container}>
-        <Image style={styles.image} source={require('./images/tryAgain.jpg')} />
-        <Text style={styles.gameover}>GAME OVER</Text>
-        <Text style={styles.score}> SCORE: {navigation.state.params.score}</Text>
+        <View style={styles.background}>
+            <Image style={styles.image} source={require('./images/tryAgain.jpg')} />
+        </View>
+        <View style={styles.details}>
+            <Text style={styles.gameover}>GAME OVER</Text>
+            <Text style={styles.score}> SCORE: {navigation.state.params.score}</Text>
+        </View>
         <TouchableOpacity style={styles.button} title="Try Again" onPress={() => navigation.navigate('Category')}>
-            <Image source={require('./images/start-button.png')} />
+            <Image source={require('./images/replaybutton.png')} />
         </TouchableOpacity>
     </View>
 )
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        display: 'flex',
-        flexDirection: 'column'
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        // display: 'flex',
+        // flexDirection: 'column'
+        flex: 1
     },
-    // image: {
-    //     flex: 1
-    // },
+    details: {
+        flex: 1,
+        marginLeft: 15,
+        marginRight: 15,
+        alignItems: 'center',
+        // justifyContent: 'center'
+    },
+    gameover: {
+        marginTop: 40,
+        fontSize: 55,
+        fontWeight: '800',
+        color: 'maroon'
+    },
+    score: {
+        fontSize: 30
+    },
+    background: {
+        flex: -1,
+        width: 20,
+        height: 10,
+        marginRight: 30,
+        marginBottom: 30
+    },
+    button: {
+        marginBottom: 30,
+        marginLeft: 30
+    }
     // gameover: {
     //     flex: 1
     // },
